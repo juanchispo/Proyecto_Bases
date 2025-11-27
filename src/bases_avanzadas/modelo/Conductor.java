71a596f9bd22f823ac65d2ef1003da37e09f3f06
juanchispo <124466430+juanchispo@users.noreply.github.com>
2025-11-27 00:18:40 -0500

    mas daos x4

--- src/bases_avanzadas/modelo/Conductor.java
+++
@@ -1,9 +0,0 @@
-package bases_avanzadas.modelo;
-
-/**
- *
- * @author Dyl
- */
-public class Conductor {
-    String Fotografia;
-}
--- src/bases_avanzadas/modelo/Tipo_servicio.java
+++
@@ -1,9 +0,0 @@
-package bases_avanzadas.modelo;
-
-/**
- *
- * @author Dyl
- */
-public enum Tipo_servicio {
-    
-}
