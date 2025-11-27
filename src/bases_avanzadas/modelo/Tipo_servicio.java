349800d6878e823c954785a3a7dcdbe882938cbd
Dylier <dyl.moyita2@gmail.com>
2025-11-26 23:15:24 -0500

    Commit de prueba

---
+++ src/bases_avanzadas/modelo/Tipo_servicio.java
@@ -0,0 +1,9 @@
+package bases_avanzadas.modelo;
+
+/**
+ *
+ * @author Dyl
+ */
+public enum Tipo_servicio {
+    
+}
---
+++ src/bases_avanzadas/modelo/Vehiculo.java
@@ -0,0 +1,12 @@
+package bases_avanzadas.modelo;
+
+/**
+ *
+ * @author Dyl
+ */
+public class Vehiculo {
+    String marca;
+    String modelo;
+    String placa;
+    Tipo_servicio tipo_servicio;
+}
