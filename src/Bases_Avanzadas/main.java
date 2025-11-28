fe3c05fc43b5fd3f3b7d39e7cdb073afaf82cc11
Dylier <dyl.moyita2@gmail.com>
2025-11-27 20:39:58 -0500

    Actualizacion Main Ejecutable

--- src/Bases_Avanzadas/main.java
+++ src/Bases_Avanzadas/main.java
@@ -1,10 +1,15 @@
 package Bases_Avanzadas;
 
+import Controlador.ControladorPrincipal;
+
 /**
  * Clase ejecutable, desde aqui se crea el controlador principal y se iniciar la
  * aplicacion
  * @author Dyl
  */
 public class main {
-    
+    public static void main(String args[]) {
+        ControladorPrincipal contP = new ControladorPrincipal();
+        contP.iniciar();
+    }
 }
