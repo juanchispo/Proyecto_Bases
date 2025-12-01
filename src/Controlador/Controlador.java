14750c514fdefc7f1cb7aed9d1d5c216e7dd5ffa
Dylier <dyl.moyita2@gmail.com>
2025-12-01 08:18:15 -0500

    Creacion de clase abstracta controlador

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -4,13 +4,33 @@
  */
 package Controlador;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
 
 /**
  *
  * @author Dyl
  */
 public abstract class Controlador implements ActionListener{    
-    public abstract void inicializarBotones();
+    public void inicializarBotones(Container contenedor) {
+        for (Component c : contenedor.getComponents()) {
+            if(c instanceof JButton){
+                ((JButton) c).addActionListener(this);
+            }else if (c instanceof JToggleButton){
+                ((JToggleButton) c).addActionListener(this);
+            }else if (c instanceof JComboBox){
+                ((JComboBox) c).addActionListener(this);
+            }else if (c instanceof JRadioButton){
+                ((JRadioButton) c).addActionListener(this);
+            }else if (c instanceof Container){
+                inicializarBotones((Container) c);
+            }
+        }
+    }
     public abstract void iniciar();
 }
