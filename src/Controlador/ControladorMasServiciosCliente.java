ce2798dcccfc89de5a158fc84e3d05eccae66c37
juanchispo <124466430+juanchispo@users.noreply.github.com>
2025-12-01 20:36:11 -0500

    controlador de cliente mas servicios x2

---
+++ src/Controlador/ControladorMasServiciosCliente.java
@@ -0,0 +1,44 @@
+package Controlador;
+
+import Modelo.Dao.DaoCliente;
+import Vista.IFrmAdministrador;
+import javax.swing.table.DefaultTableModel;
+
+public class ControladorMasServiciosCliente extends Controlador {
+
+    private IFrmAdministrador vista;
+
+    public ControladorMasServiciosCliente(IFrmAdministrador vista) {
+        this.vista = vista;
+        inicializarBotones(vista); 
+        iniciar();
+    }
+
+    @Override
+    public void iniciar() {
+        cargarClienteMasServicios();
+    }
+
+    private void cargarClienteMasServicios() {
+
+        DaoCliente dao = new DaoCliente();
+        String[] datos = dao.clienteMasServiciosVista();
+
+        DefaultTableModel modelo = (DefaultTableModel) vista.getTbClienServ().getModel();
+        modelo.setRowCount(0);
+
+        if (datos != null) {
+            modelo.addRow(new Object[]{
+                datos[0],  
+                datos[1],  
+                datos[2]   
+            });
+        }
+    }
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+    }
+}
+
