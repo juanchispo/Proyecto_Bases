c6c1edd942bc83e029e9b513d964c60a5ba77610
juanchispo <124466430+juanchispo@users.noreply.github.com>
2025-12-02 23:10:19 -0500

    Admin creado

--- src/Bases_Avanzadas/main.java
+++ src/Bases_Avanzadas/main.java
@@ -5,11 +5,15 @@ import Controlador.ControladorPrincipal;
 /**
  * Clase ejecutable, desde aqui se crea el controlador principal y se iniciar la
  * aplicacion
+ *
  * @author Dyl
  */
 public class main {
+
     public static void main(String args[]) {
         ControladorPrincipal contP = new ControladorPrincipal();
+        ControladorPrincipal cp = new ControladorPrincipal();
+        cp.iniciar();
         contP.iniciar();
     }
 }
---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,50 @@
+package Modelo;
+
+public class Usuario {
+
+    private int id;
+    private String nombre;
+    private String password;
+    private String tipo;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String password, String tipo) {
+        this.nombre = nombre;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
